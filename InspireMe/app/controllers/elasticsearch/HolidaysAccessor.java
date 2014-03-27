348ac9ce67e3824504230e6268d45a0e83f5d820
Steve <stevejohnston@live.co.uk>
2014-03-27 20:39:38 +0000

    Added extra search date

--- InspireMe/app/controllers/elasticsearch/HolidaysAccessor.java
+++ InspireMe/app/controllers/elasticsearch/HolidaysAccessor.java
@@ -22,6 +22,7 @@ import org.elasticsearch.search.SearchHitField;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * Created with IntelliJ IDEA.
