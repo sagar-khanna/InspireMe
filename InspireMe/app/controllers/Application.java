cd0a58f0ca5d41b4494bf84414a42c744f0b54c4
David Mercer <david_mercer@1591-UBU.(none)>
2014-03-27 15:21:25 +0000

    initial stab at a search page

--- InspireMe/app/controllers/Application.java
+++ InspireMe/app/controllers/Application.java
@@ -1,14 +1,17 @@
 package controllers;
 
 import play.*;
+import play.api.templates.Html;
 import play.mvc.*;
 
 import views.html.*;
+import views.html.index;
+import views.html.main;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Hello World");
+        return ok(main.render("Inspire Me Search", new Html(null)));
     }
 
     public static Result search() {
